7a217715d3cfcb0a574717f64e10693019814ed8
Zeeshan <mzeeshi187@gmail.com>
2025-04-12 17:18:09 +0500

    logos added

--- src/pages/Registrations.jsx
+++ src/pages/Registrations.jsx
@@ -21,7 +21,7 @@ const Registrations = () => {
   return (
     <div className="mt-[50px] mb-[50px] max-w-6xl mx-auto px-4">
       <h2 className="text-xl md:text-3xl font-bold text-center mb-6 text-gray-800">
-      OUR REGISTRATIONS AND PERFORMANCE
+      OUR Gallery
       </h2>
 
       {/* Masonry Grid Layout */}
